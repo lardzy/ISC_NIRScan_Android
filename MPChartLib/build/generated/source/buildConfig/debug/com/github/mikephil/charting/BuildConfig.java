c34c1afbe758bd807036695ebfa8389af36e14d1
lardzy <1019352464@qq.com>
2023-09-05 21:40:07 +0800

    废弃

---
+++ MPChartLib/build/generated/source/buildConfig/debug/com/github/mikephil/charting/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.github.mikephil.charting;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.github.mikephil.charting";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "3.0.3";
+}
