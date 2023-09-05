c300acdc0ab932cea5b7b0b35888ee443af02eba
lishuyang <lardzy@qq.com>
2023-09-05 20:11:39 +0800

    添加了自动获取纤维成分的功能
    (cherry picked from commit 43d20557ac2888c2de01882abe559405ce9db783)

--- app/src/main/java/com/Innospectra/NanoScan/ScanViewActivity.java
+++ app/src/main/java/com/Innospectra/NanoScan/ScanViewActivity.java
@@ -33,6 +33,10 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
 import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
@@ -69,8 +73,6 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 import com.opencsv.CSVWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -100,12 +102,6 @@ import static com.ISCSDK.ISCNIRScanSDK.getStringPref;
 import static com.ISCSDK.ISCNIRScanSDK.storeBooleanPref;
 import static com.ISCSDK.ISCNIRScanSDK.storeStringPref;
 import static com.Innospectra.NanoScan.DeviceStatusViewActivity.GetLampTimeString;
-
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -379,7 +375,6 @@ public class ScanViewActivity extends Activity {
     private final OkHttpClient client = new OkHttpClient();
     private Boolean isPostSampleDataRunning = false;
     private ArrayList<FabricComponent> fabricComponents = new ArrayList<>();
-    private Button scan_barcode;
 
 
     //endregion
@@ -1313,8 +1308,7 @@ public class ScanViewActivity extends Activity {
     private void InitialFabricComposition(){
         // 获得下拉框内容
         SharedPreferences sharedPreferences = getSharedPreferences("my_preferences", MODE_PRIVATE);
-        String componentsString = sharedPreferences.getString("components", "桑蚕丝,乙纶,聚酯纤维" +
-                ",棉,氨纶,动物毛纤维,芳纶,锦纶,腈纶,再生纤维素纤维,醋纤,丙纶,海藻纤维,聚酰亚胺纤维,壳聚糖纤维,其他纤维");
+        String componentsString = sharedPreferences.getString("components", "");
         if (!componentsString.equals("")){
             components = new ArrayList<>(Arrays.asList(componentsString.split(",")));
             components.add(0, "");
@@ -1356,11 +1350,6 @@ public class ScanViewActivity extends Activity {
             // 这里放置查询纤维成分的代码
             getFabricComponents(et_simple_number);
             query_fabric_components.setEnabled(false);
-        });
-        scan_barcode = (Button) findViewById(R.id.scan_barcode);
-        scan_barcode.setOnClickListener(v -> {
-            // 这里放置扫描条形码的代码
-            scanBarcode();
         });
         fileNumber = (EditText) findViewById(R.id.fileNumber);
 
@@ -1486,22 +1475,6 @@ public class ScanViewActivity extends Activity {
         editTexts.add(editTextNumber_2);
         editTexts.add(editTextNumber_3);
         editTexts.add(editTextNumber_4);
-    }
-    private void scanBarcode(){
-        new IntentIntegrator(this).initiateScan();
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
-                Toast.makeText(this, "扫描取消", Toast.LENGTH_LONG).show();
-            } else {
-                et_simple_number.setText(result.getContents());
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
     }
     public int getMaxNumberFromFilenames() {
         int maxNumber = -1; // 初始化为-1，表示未找到任何匹配的整数
@@ -2096,7 +2069,7 @@ public class ScanViewActivity extends Activity {
             return;
         }
     }
-    // 根据选择纤维部位，更新UI界面
+    // 选择纤维成分后，更新UI界面
     private void SelectedPart(int num){
         Iterator<Map.Entry<String, Double>> iterator = fabricComponents.get(num).getFiberComposition().entrySet().iterator();
         for (int i = 0; i < editTexts.size() && iterator.hasNext(); i++) {
